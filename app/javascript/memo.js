b7fb6d9ec394329812feb096b6d939ce7aa82c79
miurachikaa <chikaa719@gmail.com>
2023-01-17 08:55:19 +0900

    リクエストを処理する

--- app/javascript/memo.js
+++ app/javascript/memo.js
@@ -1,7 +1,5 @@
 function post (){
   const submit = document.getElementById("submit");
-  submit.addEventListener("click", (e) => {
-    e.preventDefault();
     const form = document.getElementById("form");
     const formData = new FormData(form);
     const XHR = new XMLHttpRequest();
